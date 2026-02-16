ee911d9c138c68fb2fecab198c0d841f360cee55
Martin <harutyun96yan@gmail.com>
2026-02-16 16:59:37 +0100

    Initial commit

---
+++ src/load_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pathlib import Path
+
+
+BASE_PATH = Path(__file__).resolve().parents[1]
+
+RAW_DATA_DIR = BASE_PATH / "data/raw"
+
+
+def load_review():
+    return pd.read_json(RAW_DATA_DIR / "Video_Games.json", lines=True)
+
+
+def load_meta():
+    return pd.read_json(RAW_DATA_DIR / "meta_Video_Games.json", lines=True)
